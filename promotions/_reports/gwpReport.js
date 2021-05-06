8c11847a412b243fc4ede5e22acd6c237aa33c6f
seanomeara96 <seanom96@gmail.com>
2021-05-06 15:11:11 +0100

    added gwp report

---
+++ promotions/_reports/gwpReport.js
@@ -0,0 +1,5 @@
+/**
+ * Need to gather stock levels
+ * 
+ * compare to last weeks figure
+ */
--- promotions/joico/applyPromotion.js
+++ promotions/joico/applyPromotion.js
@@ -1,3 +1,10 @@
 const { addLineToMany } = require("../../content/modules/update");
 
+const productIds = [];
+const lineToAdd = ``
+
 addLineToMany()
+  .then((res) => console.log(res))
+  .catch(() => {
+    console.log("something went wrong");
+  });
