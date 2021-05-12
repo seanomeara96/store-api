ce7c877abe668ffc475fdec688bf15aea49c61fc
Sean <seanom96@gmail.com>
2021-05-12 19:34:31 +0100

    rejects if data is undefined

--- config/axios-config.js
+++ config/axios-config.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const config = require("./config");
 const store = axios.create({
-  baseURL: `https://api.bigcommerce.com/stores/${config.huk}/v2`,
+  baseURL: `https://api.bigcommerce.com/stores/${config.huk}/v3`,
   headers: config.huk_xAuthTokenHeader,
 });
 module.exports = store;
--- test.js
+++ test.js
@@ -1,7 +1,41 @@
-const { getAll } = require("./requests/getAll");
+const axios = require("axios");
+const config = require("./config/config");
+const instance = axios.create({
+  baseURL: `https://api.bigcommerce.com/stores/${config.huk}/v2`,
+  headers: config.huk_xAuthTokenHeader,
+});
 
-const getAllBlogs = getAll("/blog/posts");
+const getAll =
+  (URL) =>
+  (params = {}) =>
+    new Promise((resolve, reject) => {
+      let pageNumber = 1;
+      let aggregatedData = [];
+      async function getData() {
+        try {
+          const { data } = await instance.get(URL, {
+            params: {
+              limit: 250,
+              page: pageNumber,
+              ...params,
+            },
+          });
+          if (data.data === undefined) return reject("data is undefined");
+          let dataArray = data.data;
+          if (dataArray.length) {
+            aggregatedData.push(...dataArray);
+            pageNumber++;
+            getData();
+          } else {
+            resolve(aggregatedData);
+          }
+        } catch (err) {
+          reject(err);
+        }
+      }
+      getData();
+    });
 
-getAllBlogs()
-  .then((res) => console.log(res))
-  .catch((err) => console.log(err));
+getAll("/blog/posts").then((res) => {
+  console.log(res);
+});
