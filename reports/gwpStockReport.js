1a3c113439b557db3430439791e574c5b79cf195
seanomeara96 <seanom96@gmail.com>
2021-05-14 15:46:29 +0100

    email not working

--- reports/gwpStockReport.js
+++ reports/gwpStockReport.js
@@ -2,15 +2,19 @@ const { getManyProductsBySKU } = require("../products/getManyProductsBySKU");
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 let skuArray = [
-  { sku: "GWP32" },
-  { sku: "9448" },
-  { sku: "10122" },
-  { sku: "10151" },
-  { sku: "9357" },
+  { sku: "GWP32" }, // pollution protection
+  { sku: "9448" }, // cashmere cream
+  { sku: "10122" }, // reshaping spray
+  { sku: "10151" }, // detangling comb
+  { sku: "9357" }, // caudalie hand nail cream
+  { sku: "9414" }, // one united 30ml
+  { sku: "8702" }, // clay cleanser
+  { sku: "9023" }, // tummy rub butter
 ];
 
 getManyProductsBySKU(skuArray)
   .then((res) => {
+    console.log(res)
     const data = res
       .map(
         ({ name, sku, inventory_level }) =>
@@ -18,10 +22,10 @@ getManyProductsBySKU(skuArray)
       )
       .join("\n");
     const msg = {
-      to: "sean@beautyfeatures.ie",
+      to: ["sean@beautyfeatures.ie", "brendan@beautyfeatures.ie"],
       from: "sean@beautyfeatures.ie",
       subject: "GWP Stock Report",
-      text: "Hello there",
+      text: "GWP Stock Report",
       html: data,
     };
     sgMail
