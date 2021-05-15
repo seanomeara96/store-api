37f457bee678fbab9a60afdc70964a0c4b9f5fe1
Sean <seanom96@gmail.com>
2021-05-15 18:24:25 +0100

    testing

--- scripts/misc/getAllProducts.js
+++ scripts/misc/getAllProducts.js
@@ -1,7 +1,6 @@
-const { config } = require("../../config");
-config("bf");
+require("../../config").config("bf");
 const api = require("../../api");
 api.brands
-  .getAllBrands()
+  .getBrandByName("Joico")
   .then((res) => console.log(res))
   .catch((err) => console.log(err));
