e3f3237b57a2289ac0cbd001cea3a1007dfe0bf5
Sean <seanom96@gmail.com>
2021-05-15 11:12:34 +0100

    adde product index

---
+++ api/products/index.js
@@ -0,0 +1,7 @@
+exports.content = require("./content");
+exports.getAllProducts = require("./getAllProducts");
+exports.getManyProductsBySKU = require("./getManyProductsBySKU");
+exports.getProductById = require("./getProductById");
+exports.getProductsByBrand = require("./getProductsByBrand");
+exports.getProductsByCategory = require("./getProductsByCategory");
+exports.updateProductById = require("./updateProductById");
