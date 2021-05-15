950176db42960611a626c5b7edc5000819e5eeed
Sean <seanom96@gmail.com>
2021-05-15 11:09:01 +0100

    added content index

---
+++ api/products/content/index.js
@@ -0,0 +1,8 @@
+exports.addLine = require("./addLine");
+exports.addLineToBrandProducts = require("./addLineToBrandProducts");
+exports.addLineToMany = require("./addLineToMany");
+exports.getProductDescription = require("./getProductDescription");
+exports.removeLine = require("./removeLine");
+exports.removeLineFromBrandProducts = require("./removeLineFromBrandProducts");
+exports.removeLineFromMany = require("./removeLineFromMany");
+exports.updateProductDescription = require("./updateProductDescription"); // redundant?
